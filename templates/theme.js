8b734adf3255f97cc1e87e26bb483686a033aa67
superchat0u0 <a0906085846@gmail.com>
2025-12-25 16:22:14 +0800

    初始化專案

--- templates/theme.js
+++ templates/theme.js
@@ -13,3 +13,13 @@ toggleBtn.addEventListener('click', () => {
   toggleBtn.textContent = isDark ? '🌞 淺色模式' : '🌙 深色模式';
   localStorage.setItem('theme', isDark ? 'dark' : 'light');
 });
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  const photo = document.getElementById("photo").files.length;
+  const description = document.getElementById("description").value.trim();
+
+  if (!photo && !description) {
+    e.preventDefault(); // 阻止表單送出
+    alert("請至少上傳一張照片或輸入描述！");
+  }
+});
